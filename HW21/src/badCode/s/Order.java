d6e3ffbff9950d7e7a6203870dfb22da8cbc57ee
Artem Martynov <amerta90@mail.ru>
2023-09-02 22:03:16 +0300

    HW 21. Single Responsibility Principle - SRP

---
+++ HW21/src/badCode/s/Order.java
@@ -0,0 +1,15 @@
+package badCode.s;
+
+class Order {
+    public void createOrder() {
+        // создание заказа
+    }
+
+    public void calculateTotal() {
+        // вычисление общей суммы заказа
+    }
+
+    public void sendEmailConfirmation() {
+        // отправка подтверждения заказа по электронной почте
+    }
+}
